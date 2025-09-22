0b924930c579cd1a6427412bbdec3bd10fa141d9
Pigeon1808 <160360816+Pigeon1808@users.noreply.github.com>
2025-09-22 22:56:11 +0300

    Upload
    Upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-",
-      "name": "Вход",
+      "id": "0-a3_",
+      "name": "a3_Вход",
       "levels": [
         {
           "tileSize": 256,
@@ -24,29 +24,29 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.005907916672969193,
+        "pitch": 0.06885504887213045,
+        "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": 0.6029509421360881,
-          "pitch": 0.1442744640612723,
-          "rotation": 0,
-          "target": "1--"
+          "yaw": -0.5777901650561184,
+          "pitch": 0.13705502772319988,
+          "rotation": 5.497787143782138,
+          "target": "2-a3_"
         },
         {
-          "yaw": -0.49943265119146574,
-          "pitch": 0.07464577606976164,
-          "rotation": 0,
-          "target": "2--"
+          "yaw": 0.42821050568514707,
+          "pitch": 0.06372432600059952,
+          "rotation": 7.0685834705770345,
+          "target": "1-a3_-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1--",
-      "name": "Узел учёта",
+      "id": "1-a3_-",
+      "name": "a3_Узел учёта",
       "levels": [
         {
           "tileSize": 256,
@@ -68,35 +68,35 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.522256214635247,
+        "pitch": 0.2688052262277125,
+        "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": -0.4793863962192155,
-          "pitch": 0.1666990083108466,
+          "yaw": 0.43146853790005757,
+          "pitch": 0.133623951098242,
           "rotation": 0,
-          "target": "0-"
+          "target": "2-a3_"
         },
         {
-          "yaw": 0.61800068387425,
-          "pitch": 0.11656447393180969,
-          "rotation": 0,
-          "target": "2--"
+          "yaw": -0.42983919375094537,
+          "pitch": 0.19804701596651952,
+          "rotation": 5.497787143782138,
+          "target": "0-a3_"
         },
         {
-          "yaw": 1.9801493056840878,
-          "pitch": 0.20456267175163134,
-          "rotation": 0,
-          "target": "3--"
+          "yaw": 1.4960246134723212,
+          "pitch": 0.20018794300928278,
+          "rotation": 0.7853981633974483,
+          "target": "3-a3_"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2--",
-      "name": "Переходной канал",
+      "id": "2-a3_",
+      "name": "a3_Переход",
       "levels": [
         {
           "tileSize": 256,
@@ -124,29 +124,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.0428349652508313,
-          "pitch": 0.10237141864654298,
+          "yaw": 0.41159092180119927,
+          "pitch": 0.09540496754361172,
           "rotation": 0,
-          "target": "1--"
+          "target": "1-a3_-"
         },
         {
-          "yaw": -0.9874175709902175,
-          "pitch": 0.20997600791668525,
-          "rotation": 0,
-          "target": "3--"
+          "yaw": -0.6269688378918552,
+          "pitch": 0.17255966440139758,
+          "rotation": 5.497787143782138,
+          "target": "3-a3_"
         },
         {
-          "yaw": 0.9919993963652747,
-          "pitch": 0.15955878180409222,
-          "rotation": 0,
-          "target": "0-"
+          "yaw": 0.9467459926561883,
+          "pitch": 0.06617700670136628,
+          "rotation": 7.0685834705770345,
+          "target": "0-a3_"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3--",
-      "name": "Узел ввода",
+      "id": "3-a3_",
+      "name": "a3_Ввод",
       "levels": [
         {
           "tileSize": 256,
@@ -168,32 +168,32 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.44302987138665273,
+        "pitch": 0.25938509514829455,
+        "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": 0.24113083800878599,
-          "pitch": 0.14944918903443138,
-          "rotation": 0,
-          "target": "2--"
+          "yaw": 0.5043222702844474,
+          "pitch": 0.17004131546425683,
+          "rotation": 0.7853981633974483,
+          "target": "2-a3_"
         },
         {
-          "yaw": -1.081933494646135,
-          "pitch": -0.0003711796411245416,
-          "rotation": 0,
-          "target": "1--"
+          "yaw": -1.0740010946290361,
+          "pitch": 0.03986173310148011,
+          "rotation": 5.497787143782138,
+          "target": "1-a3_-"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "ITP",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": false,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
